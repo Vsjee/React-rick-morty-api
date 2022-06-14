06464ec2d7d978eb7adcb1fb5f74a18da3943f79
david hernandez <davidfelipehernandez1@gmail.com>
2022-06-14 13:00:33 -0500

    reactMortyAPI(#3)

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ function App() {
   return (
     <div>
       <nav style={{
-        backgroundColor: "#81f",
+        backgroundColor: "#000",
         width: '100% 1000px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        padding: '1.5rem',
+        padding: '1.8rem',
         margin: '-8px',
         gap: '1.5rem',
       }}>
-        <Link to='/home'>Home</Link>
-        <Link to='/rick'>Rick Sanchez</Link>
-        <Link to='/morty'>Morty Smith</Link>
-        <Link to='/Annie'>Annie</Link>
-        <Link to='/Antenna'>Antenna Rick</Link>
+        <Link to='/home' className='link--item'>Home</Link>
+        <Link to='/rick' className='link--item'>Rick Sanchez</Link>
+        <Link to='/morty' className='link--item'>Morty Smith</Link>
+        <Link to='/Annie' className='link--item'>Annie</Link>
+        <Link to='/Antenna' className='link--item'>Antenna Rick</Link>
       </nav>
       <Outlet />
     </div>
